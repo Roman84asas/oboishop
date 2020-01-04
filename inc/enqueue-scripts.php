8dc15b1231261920f2a4725438ae31d56a6d461f
RomanMereneanu <asasin84@mail.ru>
2020-01-04 21:42:30 +0200

    add new project

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -3,11 +3,14 @@
  * Enqueue scripts and styles.
  */
 function oboishop_scripts() {
+    wp_enqueue_style( 'general', get_template_directory_uri() . '/assets/css/general.css', array());
     wp_enqueue_style( 'oboishop-style', get_stylesheet_uri() );
+
 
     wp_enqueue_script( 'oboishop-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20151215', true );
 
     wp_enqueue_script( 'oboishop-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
+    wp_enqueue_script( 'script-main', get_template_directory_uri() . '/assets/js/script.js', array(), '20151215', true );
 
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
