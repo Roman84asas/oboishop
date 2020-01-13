65527ec47005b9c45bfd415b22995b38571f0da9
RomanMereneanu <asasin84@mail.ru>
2020-01-13 21:28:16 +0200

    add category 1

--- templates/page-home.php
+++ templates/page-home.php
@@ -34,11 +34,35 @@ get_header(); ?>
         </div>
     </section>
 
-    <section class="sidebar">
-        сайдбар с категориями товаров
-    </section>
-    <section class="all-show-info">
-        Статьи с информацией и фото
+    <section class="content-shop">
+        <section class="sidebar">
+            <ul class="homepage-category">
+                <?php
+                $prod_cat_args = array(
+                    'taxonomy'    => 'product_cat',
+                    'orderby'     => 'id',
+                    'parent'      => 0
+                );
+
+                $woo_categories = get_categories( $prod_cat_args );
+                foreach ( $woo_categories as $woo_cat ) {
+                    $woo_cat_id = $woo_cat->term_id;
+                    $woo_cat_name = $woo_cat->name;
+                    $woo_cat_slug = $woo_cat->slug;
+                    echo '<li class="main-cat-item">';
+                    $category_thumbnail_id = get_term_meta($woo_cat_id, 'thumbnail_id', true);
+                    $thumbnail_image_url = wp_get_attachment_image_src( $category_thumbnail_id, 'medium')[0];
+                    echo '<a href="' . get_term_link( $woo_cat_id, 'product_cat' ) . '"><img src="' . $thumbnail_image_url . '"/> <h4>' . $woo_cat_name . '</h4></a>';
+                    echo "</li>\n";
+                }
+
+                ?>
+                <li class="main-cat-item"><a href="#"><h4>Калькулятор</h4></a></li>
+            </ul>
+        </section>
+        <section class="all-show-info">
+            Статьи с информацией и фото
+        </section>
     </section>
 
 <?php get_footer();
