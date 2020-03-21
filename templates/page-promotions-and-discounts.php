6d48770527b475c509b46d0e5fe072ba7dbc72ac
RomanMereneanu <asasin84@mail.ru>
2020-03-21 20:56:29 +0200

    refactoring content

--- templates/page-promotions-and-discounts.php
+++ templates/page-promotions-and-discounts.php
@@ -13,7 +13,7 @@ get_header(); ?>
     <section class="content-shop">
         <?php get_template_part( 'template-parts/section-sidebar', 'page' ); ?>
         <section class="all-show-prom">
-
+            Акции и скидки
         </section>
     </section>
 
