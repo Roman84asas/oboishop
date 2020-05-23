2285e0af5fc13f3f1746a4bfe13956260ced4042
RomanMereneanu <asasin84@mail.ru>
2020-05-23 11:36:41 +0300

    выполняется Контакты

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -11,6 +11,9 @@ function oboishop_scripts() {
 	if(is_page( 340 )){
 		wp_enqueue_style( 'page-about', get_template_directory_uri() . '/assets/css/about.css', array());
 	}
+	if(is_page( 355 )){
+		wp_enqueue_style( 'page-contact', get_template_directory_uri() . '/assets/css/contact.css', array());
+	}
     wp_enqueue_style( 'font-icon', get_template_directory_uri() . '/assets/css/all.min.css', array());
     wp_enqueue_style( 'owlcarousel', get_template_directory_uri() . '/libs/owl.carousel/dist/assets/owl.carousel.min.css', array());
     wp_enqueue_style( 'owlcarousel2', get_template_directory_uri() . '/libs/owl.carousel/dist/assets/owl.theme.default.min.css', array());
--- templates/page-contact.php
+++ templates/page-contact.php
@@ -6,7 +6,7 @@ defined( 'ABSPATH' ) || exit;
 
 get_header(); ?>
 
-    <section class="content-shop">
+    <section class="content_contact">
         <?php get_template_part( 'template-parts/section-sidebar', 'page' ); ?>
         <section class="all-show-info">
             Секция Контакт
