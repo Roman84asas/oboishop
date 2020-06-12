95d423d684f299b4aacd38cbfcfeb3723a71a04e
RomanMereneanu <asasin84@mail.ru>
2020-06-12 21:59:55 +0300

    выполнено

--- woocommerce/includes/wc_fields_for_prod.php
+++ woocommerce/includes/wc_fields_for_prod.php
@@ -1,15 +1,45 @@
 <?php
 
 function wc_fields_for_prod() {
-    $get_tovar = get_field('tovar');
-    $get_articul = get_field('articul');
-    $get_interjer = get_field('interjer');
-    $get_tovar = get_field('tovar');
-    $get_tovar = get_field('tovar');
-    $get_tovar = get_field('tovar');
-    $get_tovar = get_field('tovar');
-    $get_tovar = get_field('tovar');
-    $get_tovar = get_field('tovar');
+    $get_tovar         = get_field('tovar');
+    $get_articul       = get_field('articul');
+    $get_interjer      = get_field('interjer');
+    $get_class         = get_field('class');
+    $get_still         = get_field('still');
+    $get_land_brend    = get_field('land_brend');
+    $get_lenght_mm     = get_field('lenght_mm');
+    $get_widht_mm      = get_field('widht_mm');
+    $get_tolshina_mm   = get_field('tolshina_mm');
+    $get_collection    = get_field('collection');
+    $get_garantija     = get_field('garantija');
+    $get_risunok       = get_field('risunok');
+    $get_size_risunka  = get_field('size_risunka');
+    $get_type_pokr     = get_field('type_pokr');
+    $get_type_soedin   = get_field('type_soedin');
+    $get_type_room     = get_field('type_room');
+    $get_type_light    = get_field('type_light');
+    $get_type_cokol    = get_field('type_cokol');
+    $get_type_tovara   = get_field('type_tovara');
+    $get_pokritie      = get_field('pokritie');
+    $get_vid_mater     = get_field('vid_mater');
+    $get_vlagostoikost = get_field('vlagostoikost');
+    $get_zamk_sist     = get_field('zamk_sist');
+    $get_faska         = get_field('faska');
+    $get_coll_polos    = get_field('coll_polos');
+    $get_plosh_v_upak  = get_field('plosh_v_upak');
+    $get_faktura       = get_field('faktura');
+    $get_pult_du       = get_field('pult_du');
+    $get_naprjaj_V     = get_field('naprjaj_V');
+    $get_forma_plafona = get_field('forma_plafona');
+    $get_stepen_zash   = get_field('stepen_zash');
+    $get_mat_armaturi  = get_field('mat_armaturi');
+    $get_lamp_in_compl = get_field('lamp_in_compl');
+    $get_coll_lamp     = get_field('coll_lamp');
+    $get_mat_plafon    = get_field('mat_plafon');
+    $get_ligth_row_lm  = get_field('ligth_row_lm');
+    $get_power_lamp_W  = get_field('power_lamp_W');
+    $get_color_temp_K  = get_field('color_temp_K');
+    $get_all_power_W   = get_field('all_power_W');
 
 
     if(empty($img)){ ?>
